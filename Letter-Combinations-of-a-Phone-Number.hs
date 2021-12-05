286598c24e43f5c311ec075f3c0ad1e0914f5010
Shaobo <polarishehn@gmail.com>
2021-12-04 17:23:01 -0800

    Create Letter-Combinations-of-a-Phone-Number.hs

---
+++ Letter-Combinations-of-a-Phone-Number.hs
@@ -0,0 +1,14 @@
+
+letterCombinations :: [Char] -> [[Char]]
+letterCombinations [] = [[]]
+letterCombinations (f:r) = (:) <$> getLetter f <*> letterCombinations r where
+  getLetter '2' = ['a', 'b', 'c']
+  getLetter '3' = ['d', 'e', 'f']
+  getLetter '4' = ['g', 'h', 'j']
+  getLetter '5' = ['j', 'k', 'l']
+  getLetter '6' = ['m', 'n', 'o']
+  getLetter '7' = ['p', 'q', 'r', 's']
+  getLetter '8' = ['t', 'u', 'v']
+  getLetter '9' = ['w', 'x', 'y', 'z']
+
+main = putStrLn $ show $ letterCombinations "23"
