def8755561669868f85f7f0be5bc8cb1e9ec8782
Shaobo <polarishehn@gmail.com>
2021-12-04 17:32:38 -0800

    Update Letter-Combinations-of-a-Phone-Number.hs

--- Letter-Combinations-of-a-Phone-Number.hs
+++ Letter-Combinations-of-a-Phone-Number.hs
@@ -1,14 +1,15 @@
-
 letterCombinations :: [Char] -> [[Char]]
-letterCombinations [] = [[]]
-letterCombinations (f:r) = (:) <$> getLetter f <*> letterCombinations r where
-  getLetter '2' = ['a', 'b', 'c']
-  getLetter '3' = ['d', 'e', 'f']
-  getLetter '4' = ['g', 'h', 'j']
-  getLetter '5' = ['j', 'k', 'l']
-  getLetter '6' = ['m', 'n', 'o']
-  getLetter '7' = ['p', 'q', 'r', 's']
-  getLetter '8' = ['t', 'u', 'v']
-  getLetter '9' = ['w', 'x', 'y', 'z']
+letterCombinations [] = []
+letterCombinations [d] = [[l] | l <- getLetter d]
+letterCombinations (f:r) = (:) <$> getLetter f <*> letterCombinations r
+
+getLetter '2' = ['a', 'b', 'c']
+getLetter '3' = ['d', 'e', 'f']
+getLetter '4' = ['g', 'h', 'j']
+getLetter '5' = ['j', 'k', 'l']
+getLetter '6' = ['m', 'n', 'o']
+getLetter '7' = ['p', 'q', 'r', 's']
+getLetter '8' = ['t', 'u', 'v']
+getLetter '9' = ['w', 'x', 'y', 'z']
 
 main = putStrLn $ show $ letterCombinations "23"
